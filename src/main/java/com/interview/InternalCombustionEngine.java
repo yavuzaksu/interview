f97dca48a98b54eb6c83f056d6a69b012d87ad52
Yavuz <yavuz.aksu@yahoo.com>
2015-10-14 17:00:07 +0100

    refactoring engines

--- src/main/java/com/interview/InternalCombustionEngine.java
+++ src/main/java/com/interview/InternalCombustionEngine.java
@@ -1,81 +1,21 @@
 package com.interview;
 
-public class InternalCombustionEngine implements Engine{
+public class InternalCombustionEngine  extends AbstractEngine implements Engine{
 
     public static final int BATCH_SIZE = 0;
 
-    private boolean running;
-    private int fuelLevel;
-    private FuelType fuelType;
-    private FuelType requiredFuelType;
-
     public InternalCombustionEngine(final FuelType requiredFuelType) {
 
-        if(isRightFuelForTheEngine(requiredFuelType)) {
-            this.requiredFuelType = requiredFuelType;
-        }else{
-            throw new IllegalArgumentException("Not a valid Fuel type to initialise the engine.");
-        }
-
-        running = false;
-        fuelLevel = 0;
+       super(requiredFuelType);
     }
 
-    public void start() {
-        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
-            running = true;
-        } else {
-            throw new IllegalStateException("Not able to start engine.");
-        }
-    }
-
-    public void stop() {
-        running = false;
-    }
-
-    public boolean isRunning() {
-        return running;
-    }
-
-    public void fill(FuelType fuelType, int fuelLevel) {
-
-        if(isRightFuelForTheEngine(fuelType)) {
-            this.fuelType = fuelType;
-        }else{
-            throw new IllegalArgumentException("Not a valid Fuel type to fill the engine.");
-        }
-
-        if (fuelLevel >= 0 && fuelLevel <= 100) {
-            this.fuelLevel = fuelLevel;
-        }
-        else if (fuelLevel > 100) {
-            this.fuelLevel = 100;
-        }
-        else {
-            this.fuelLevel = 0;
-        }
-
-        this.fuelType = fuelType;
-    }
-
-    public int getFuelLevel() {
-        return fuelLevel;
-    }
-
-    public FuelType getRequiredFuelType() {
-        return  requiredFuelType;
-    }
-    public FuelType getFuelType() {
-        return  fuelType;
-    }
 
     public double getBatchCost() {
         return fuelType.equals(FuelType.PETROL) ? 9.0 : 12.0;
     }
 
 
-
-    private boolean isRightFuelForTheEngine(FuelType fuelType){
+    boolean isRightFuelForTheEngine(FuelType fuelType){
         return fuelType.equals(FuelType.PETROL) || fuelType.equals(FuelType.DIESEL) ? true : false;
     }
 }
--- src/main/java/com/interview/SteamEngine.java
+++ src/main/java/com/interview/SteamEngine.java
@@ -1,72 +1,11 @@
 package com.interview;
 
-public class SteamEngine implements Engine {
+public class SteamEngine extends AbstractEngine implements Engine {
 
     public static final int BATCH_SIZE = 2;
 
-    private boolean running;
-    private int fuelLevel;
-    private FuelType fuelType;
-    private FuelType requiredFuelType;
-
     public SteamEngine(FuelType requiredFuelType) {
-
-        if(isRightFuelForTheEngine(requiredFuelType)) {
-            this.requiredFuelType = requiredFuelType;
-        }else{
-            throw new IllegalArgumentException("Not a valid Fuel type to initialise the engine.");
-        }
-        running = false;
-        fuelLevel = 0;
-    }
-
-    public void start() {
-        if (fuelLevel > 0 && requiredFuelType.equals(fuelType)) {
-            running = true;
-        } else {
-            throw new IllegalStateException("Not able to start engine.");
-        }
-    }
-
-    public void stop() {
-        running = false;
-    }
-
-    public boolean isRunning() {
-        return running;
-    }
-
-
-    public void fill(FuelType fuelType, int fuelLevel) {
-
-        if(isRightFuelForTheEngine(fuelType)) {
-            this.fuelType = fuelType;
-        }else{
-            throw new IllegalArgumentException("Not a valid Fuel type to fill the engine.");
-        }
-
-        if (fuelLevel >= 0 && fuelLevel <= 100) {
-            this.fuelLevel = fuelLevel;
-        }
-        else if (fuelLevel > 100) {
-            this.fuelLevel = 100;
-        }
-        else {
-            this.fuelLevel = 0;
-        }
-
-        this.fuelType = fuelType;
-    }
-
-    public int getFuelLevel() {
-        return fuelLevel;
-    }
-
-    public FuelType getRequiredFuelType() {
-        return  requiredFuelType;
-    }
-    public FuelType getFuelType() {
-        return  fuelType;
+       super(requiredFuelType);
     }
 
     public double getBatchCost() {
@@ -74,11 +13,7 @@ public class SteamEngine implements Engine {
         return fuelType.equals(FuelType.COAL) ? 5.65 : 4.35;
     }
 
-    public int getBatchSize() {
-        return 0;
-    }
-
-    private boolean isRightFuelForTheEngine(FuelType fuelType){
+    boolean isRightFuelForTheEngine(FuelType fuelType){
         return fuelType.equals(FuelType.WOOD) || fuelType.equals(FuelType.COAL) ? true : false;
     }
 }
