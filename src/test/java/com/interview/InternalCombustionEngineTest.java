a0e2f49063b5968f7c49e339b50e482d32de3039
System Administrator <root@Yavuzs-MacBook-Pro.local>
2015-10-14 14:46:35 +0100

    tests

--- src/test/java/com/interview/InternalCombustionEngineTest.java
+++ src/test/java/com/interview/InternalCombustionEngineTest.java
@@ -1,12 +1,19 @@
 package com.interview;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class InternalCombustionEngineTest {
+
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
 
     @Test
     public void construction() {
@@ -19,7 +26,18 @@ public class InternalCombustionEngineTest {
     }
 
     @Test
+    public void startBad() {
+        InternalCombustionEngine engine = new InternalCombustionEngine(FuelType.DIESEL);
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage(equalTo("Not able to start engine."));
+        engine.start();
+    }
+
+    @Test
     public void start() {
+        InternalCombustionEngine engine = new InternalCombustionEngine(FuelType.DIESEL);
+        engine.fill(FuelType.DIESEL, 50);
+        engine.start();
     }
 
     @Test
